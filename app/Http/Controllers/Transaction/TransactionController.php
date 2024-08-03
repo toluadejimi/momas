cb9e41f16d417304a04bdf91556ceceab7973c71
Jimmy <toluadejimi@gmail.com>
2024-08-03 15:19:39 +0100

    pusgggghhh

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -59,19 +59,73 @@ class TransactionController extends Controller
 
 
         if($request->pay_type == 'paystack'){
+
+
+            $fl = Setting::where('id', 1)->first();
+            $flkey['flutterwave_secret'] = $fl->flutterwave_secret;
+            $flkey['flutterwave_public'] = $fl->flutterwave_public;
+            $paystackkey = $fl->paystack_secret;
+            $pkkey['paystack_public'] = $fl->paystack_public;
+
+
             $trx_id = "FUND".random_int(0000000, 9999999);
             $email = Auth::user()->email;
-            $trx =  new Transaction();
-            $trx->user_id = Auth::id();
-            $trx->pay_type = "paystack";
-            $trx->amount = $request->amount;
-            $trx->trx_id = $trx_id;
-            $trx->save();
 
-            return response()->json([
-                'status' => true,
-                'url' => url('')."/pay-paystack?amount=$request->amount&trx_id=$trx_id&email=$email"
-            ], 200);
+
+            $databody = array(
+                "amount" => $request->amount * 100,
+                "email" => $email,
+            );
+
+            $body = json_encode($databody);
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.paystack.co/transaction/initialize',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $body,
+                CURLOPT_HTTPHEADER => array(
+                    'Accept: application/json',
+                    'Content-Type: application/json',
+                    'Authorization: Bearer ' . $paystackkey,
+                ),
+            ));
+
+            $var = curl_exec($curl);
+            curl_close($curl);
+            $var = json_decode($var);
+            $status = $var->status;
+
+            if($status == true){
+                $trx =  new Transaction();
+                $trx->user_id = Auth::id();
+                $trx->pay_type = "paystack";
+                $trx->amount = $request->amount;
+                $trx->trx_id = $trx_id;
+                $trx->save();
+
+                return response()->json([
+                    'status' => true,
+                    'url' => $var->data->authorization_url
+                ], 200);
+
+            }
+
+
+
+            $code = 422;
+            $message="Payment not available at the moment, Kindly select other payment option";
+            error($message, $code);
+
+
+
+
         }
 
 
