bda52ec9b4f872b39fc4f2e28915887eee65cb14
Jimmy <toluadejimi@gmail.com>
2024-11-07 09:45:45 +0100

    pusgggghhh

--- app/Http/Controllers/Service/ServiceController.php
+++ app/Http/Controllers/Service/ServiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Service;
 
 use App\Http\Controllers\Controller;
 use App\Models\Estate;
+use App\Models\EstateService;
 use App\Models\Job;
 use App\Models\Rating;
 use App\Models\Service;
@@ -17,7 +18,7 @@ class ServiceController extends Controller
     {
 
         $data['estate'] = Estate::where('status', 2)->get()->makeHidden(['created_at', 'updated_at']);
-        $data['service'] = Service::where('status', 2)->where('estate_id', auth::user()->estate_id)->get()->makeHidden(['created_at', 'updated_at']);
+        $data['service'] = EstateService::where('status', 2)->where('estate_id', auth::user()->estate_id)->get()->makeHidden(['created_at', 'updated_at']);
 
         return response()->json([
             'status' => true,
@@ -30,7 +31,7 @@ class ServiceController extends Controller
 
     public function service_search(request $request)
     {
-      $jobs =   Job::where('estate_id', $request->estate_id)->where('service_id', $request->service_id)->get()->makeHidden(['created_at', 'updated_at']) ?? null;
+      $jobs =   EstateService::where('estate_id', $request->estate_id)->where('service_id', $request->service_id)->get()->makeHidden(['created_at', 'updated_at']) ?? null;
 
       if($jobs == null){
           $code = 401;
