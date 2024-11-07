4763f37efb67b0ee056a093ca062f44bce3da88d
Jimmy <toluadejimi@gmail.com>
2024-11-07 09:44:36 +0100

    pusgggghhh

--- app/Http/Controllers/Service/ServiceController.php
+++ app/Http/Controllers/Service/ServiceController.php
@@ -17,7 +17,7 @@ class ServiceController extends Controller
     {
 
         $data['estate'] = Estate::where('status', 2)->get()->makeHidden(['created_at', 'updated_at']);
-        $data['service'] = Service::where('status', 2)->get()->makeHidden(['created_at', 'updated_at']);
+        $data['service'] = Service::where('status', 2)->where('estate_id', auth::user()->estate_id)->get()->makeHidden(['created_at', 'updated_at']);
 
         return response()->json([
             'status' => true,
