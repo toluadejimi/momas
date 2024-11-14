bf2cbd47992b658693db4fd99d45d76bd39440ba
Jimmy <toluadejimi@gmail.com>
2024-11-14 18:18:53 +0100

    pusgggghhh

--- app/Http/Controllers/Admin/DashboardContoller.php
+++ app/Http/Controllers/Admin/DashboardContoller.php
@@ -89,8 +89,8 @@ class DashboardContoller extends Controller
 
         if(auth::user()->role == 0){
 
-            $data['users'] = User::latest()->where('status', 2)->where('role', 3)->count();
-            $data['users_lists'] = User::paginate('20');
+            $data['users'] = User::latest()->where('status', 2)->count();
+            $data['users_lists'] = User::latest()->where('role'!= 3)->paginate('20');
             return view('admin/user/user-list', $data);
 
         } elseif(auth::user()->role == 1){
