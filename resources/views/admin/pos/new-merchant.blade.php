0c7edc97848b1f7f8c0c2052f3c8ee8a60a97c69
Jimmy <toluadejimi@gmail.com>
2024-12-19 16:08:29 +0100

    pushh

---
+++ resources/views/admin/pos/new-merchant.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.main')
+@section('content')
+
+
+    @if(auth::user()->role == 0)
+        <div class="content">
+
+            <!-- Start Content-->
+            <div class="container-fluid">
+
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session()->get('message') }}
+                    </div>
+                @endif
+                @if (session()->has('error'))
+                    <div class="alert alert-danger">
+                        {{ session()->get('error') }}
+                    </div>
+                @endif
+
+
+
+
+                <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
+                    <div class="flex-grow-1">
+                        <h4 class="fs-18 fw-semibold m-0">Add New Tariff</h4>
+                    </div>
+                </div>
+
+
+                <div class="row">
+
+                    <div class="card">
+
+                        <div class="card-body">
+
+                            <form action="add-new-Tariff" method="post">
+                                @csrf
+
+                                <div class="row">
+
+                                    <h6 class="d-flex justify-content-start my-4">Tariff Information</h6>
+
+                                    <div class="col-xl-4 col-sm-12">
+                                        <label class="my-2">Tariff Title</label>
+                                        <input type="text" value="TF" name="title" class="form-control" required>
+
+
+                                    </div>
+
+
+
+
+                                    <div class="col-xl-4 col-sm-12">
+                                        <label class="my-2">Tariff Index</label>
+                                        <select class="form-control" name="tariff_index" required>
+                                            <option value="">---Select Index-----</option>
+                                            @php
+                                                for ($i = 1; $i <= 99; $i++) {
+                                                    echo "<option value=\"$i\">$i</option>";
+                                                }
+                                            @endphp
+
+                                        </select>
+
+                                    </div>
+
+
+
+
+
+                                    <hr class="my-4">
+
+
+                                    <button type="submit" class="col-xl-2 col-sm-12 d-flex btn btn-primary">
+                                        Create
+                                    </button>
+
+                                </div>
+
+
+                            </form>
+
+
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+
+        </div> <!-- container-fluid -->
+    @elseif(auth::user()->role == 1)
+    @elseif(auth::user()->role == 2)
+    @elseif(auth::user()->role == 3)
+      
+    @elseif(auth::user()->role == 4)
+    @elseif(auth::user()->role == 5)
+    @else
+    @endif
+
+
+
+@endsection
