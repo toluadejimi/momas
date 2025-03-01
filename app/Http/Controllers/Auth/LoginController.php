1085f0542415bcda065b1d7f141462a8a0c651af
Jimmy <toluadejimi@gmail.com>
2025-03-01 13:02:51 +0100

    pusgggghhh

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -68,9 +68,9 @@ class LoginController extends Controller
                 ->first();
 
             if($admin_fee_get){
-                $admin_fee =  "1";
+                $admin_fee =  "0";
             }else{
-                $admin_fee = "0";
+                $admin_fee = "1";
             }
 
 
@@ -219,9 +219,9 @@ class LoginController extends Controller
             ->first();
 
         if($admin_fee_get){
-            $admin_fee =  "1";
+            $admin_fee =  "0";
         }else{
-            $admin_fee = "0";
+            $admin_fee = "1";
         }
 
 
