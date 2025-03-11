348724dc9c29ea84f8e8657a4cfcea7de4e1a369
Jimmy <toluadejimi@gmail.com>
2025-03-11 16:10:02 +0100

    pusgggghhh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:check-utility')->everyMinute();
-
+        $schedule->command('app:check-utility')->monthlyOn(1, '12:00');
     }
 
     /**
