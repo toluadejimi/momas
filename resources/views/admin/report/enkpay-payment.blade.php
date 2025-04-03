306e3c4b9c15cfa3ce492d371b271dc70881f3ec
Jimmy <toluadejimi@gmail.com>
2025-04-03 04:40:20 +0100

    pusgggghhh

--- resources/views/admin/report/enkpay-payment.blade.php
+++ resources/views/admin/report/enkpay-payment.blade.php
@@ -67,6 +67,8 @@
 
                                     <div class="d-flex align-items-center">
                                         <h3 class="mb-0 fs-24 text-black me-2">₦{{number_format($balance)}}</h3>
+                                        <a href="#" data-bs-toggle="modal"
+                                           data-bs-target="#staticBackdrop" class="btn btn-primary">Fund Wallet</a>
 
                                     </div>
 
@@ -106,88 +108,54 @@
                     </div>
 
 
-                    <div class="col-md-6 col-xl-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="widget-first">
 
-                                    <div class="d-flex align-items-center mb-2">
-                                        <div
-                                            class="bg-secondary-subtle rounded-circle p-2 me-2 border border-dashed border-secondary">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                                 viewBox="0 0 640 512">
-                                                <path fill="#963b68"
-                                                      d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64s-64 28.7-64 64s28.7 64 64 64m448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64s-64 28.7-64 64s28.7 64 64 64m32 32h-64c-17.6 0-33.5 7.1-45.1 18.6c40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64m-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32S208 82.1 208 144s50.1 112 112 112m76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2m-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4"/>
-                                            </svg>
+                </div>
+
+
+
+                <div class="col-xl-6">
+                    <div class="card">
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop"
+                             data-bs-backdrop="static" data-bs-keyboard="false"
+                             tabindex="-1" aria-labelledby="staticBackdropLabel"
+                             aria-hidden="true">
+
+
+
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5"
+                                            id="staticBackdropLabel">Fund Wallet</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                    </div>
+
+                                    <form action="/fund_wallet" method="POST" enctype="multipart/form-data">
+                                        @csrf
+
+                                        <div class="modal-body">
+
+                                            <label class="my-1">Enter Amount to fund </label>
+                                            <input type="number"  class="form-control mb-3" name="amount" required>
+
+
                                         </div>
 
-                                        <p class="mb-0 text-dark fs-15">Total Funded</p>
-                                    </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close
+                                            </button>
+                                            <button type="submit" class="btn btn-primary">Continue</button>
+                                        </div>
 
-                                    <div class="d-flex align-items-center">
-                                        <h3 class="mb-0 fs-24 text-black me-2">₦{{number_format($total_withdrawal)}}</h3>
-                                        <a href="#" data-bs-toggle="modal"
-                                           data-bs-target="#staticBackdrop" class="btn btn-primary">Fund Wallet</a>
-
-                                    </div>
-
-
-
-
-                                    <div class="col-xl-6">
-                                        <div class="card">
-                                            <!-- Modal -->
-                                            <div class="modal fade" id="staticBackdrop"
-                                                 data-bs-backdrop="static" data-bs-keyboard="false"
-                                                 tabindex="-1" aria-labelledby="staticBackdropLabel"
-                                                 aria-hidden="true">
-
-
-
-                                                <div class="modal-dialog modal-dialog-centered">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h1 class="modal-title fs-5"
-                                                                id="staticBackdropLabel">Fund Wallet</h1>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                                    aria-label="Close"></button>
-                                                        </div>
-
-                                                        <form action="/fund_wallet" method="POST" enctype="multipart/form-data">
-                                                            @csrf
-
-                                                            <div class="modal-body">
-
-                                                                <label class="my-1">Enter Amount to fund </label>
-                                                                <input type="number"  class="form-control mb-3" name="amount" required>
-
-
-                                                            </div>
-
-                                                            <div class="modal-footer">
-                                                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close
-                                                                </button>
-                                                                <button type="submit" class="btn btn-primary">Continue</button>
-                                                            </div>
-
-                                                        </form>
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div> <!-- end card -->
-                                    </div>
-
-
-
+                                    </form>
 
                                 </div>
                             </div>
                         </div>
-                    </div>
-
+                    </div> <!-- end card -->
                 </div>
-
 
 
 
