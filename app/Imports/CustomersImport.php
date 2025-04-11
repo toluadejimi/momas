507d63472dde7d9e4c44eee0a7352894e9b23803
Jimmy <toluadejimi@gmail.com>
2025-04-11 12:23:33 +0100

    pusgggghhh

--- app/Imports/CustomersImport.php
+++ app/Imports/CustomersImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Models\Estate;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Http\Request;
@@ -35,10 +36,13 @@ class CustomersImport implements ToModel, WithHeadingRow
 
 
             if (Auth::user()->role == 3) {
+
+                $email = Str::random(10) . '@example.com';
+
                 return new User([
                     'first_name'   => $row['firstname'],
                     'last_name'    => $row['lastname'],
-                    'email'        => $row['email'],
+                    'email'        => $row['email'] ?? $email,
                     'address'      => $row['address'],
                     'state'        => $row['state'],
                     'phone'        => "+234".$row['phone'],
@@ -57,11 +61,13 @@ class CustomersImport implements ToModel, WithHeadingRow
 
 
                 $estate_name = Estate::where('id', $this->id)->first()->title;
+                $email = Str::random(10) . '@example.com';
+
                 return new User([
 
                     'first_name'   => $row['firstname'],
                     'last_name'    => $row['lastname'],
-                    'email'        => $row['email'],
+                    'email'        => $row['email'] ?? $email,
                     'address'      => $row['address'],
                     'state'        => $row['state'],
                     'phone'        => "+234".$row['phone'],
